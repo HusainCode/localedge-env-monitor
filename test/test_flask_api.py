067d773f9de99b5bddb26ac91b8ece20bbd79dfe
HusainCode <alshaikhahmed@hotmail.com>
2025-05-20 22:27:14 -0500

    Working on TestFlaskAPI

--- test/test_flask_api.py
+++ test/test_flask_api.py
@@ -1,9 +1,14 @@
 import pytest
+from app import create_app
 
 class TestFlaskApi:
-    def test_read(self):
-        pass
+    @pytest.fixture(autouse=True)
+    def setup_app(self):
+        self.app = create_app().test_client()
 
+    def test_read(self):
+        response = self.app.get("/")
+        assert response.status_code == 200
 
 if __name__ == '__main__':
     pytest.main()
