2488dea88d4761ead60c869a8131c5e45d1e07e2
HusainCode <alshaikhahmed@hotmail.com>
2025-05-21 21:45:53 -0500

    log testing

--- test/test_logger.py
+++ test/test_logger.py
@@ -1,9 +1,17 @@
+from app_logging.decorators.log_decorator import LogDecorator
 import pytest
 
-class TestLogger:
-    def test_read(self):
-        pass
+@LogDecorator
+def just_function():
+    return "Done"
 
+class TestLogger:
+    def test_read(self, capsys):
+        result = just_function()
+        captured = capsys.readouterr()
+
+        # assert "Logging" in captured.out
+        assert result == "Done"
 
 if __name__ == '__main__':
     pytest.main()
